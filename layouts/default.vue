7b317d5be022786665d65b0c744a019e817edc6e
bskimball <brian.kimball@bdkonline.com>
2017-07-21 13:43:31 -0400

    Create default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <main-menu></main-menu>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+  * {
+    font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+  }
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+</style>
